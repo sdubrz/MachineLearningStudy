27b55a791c1bd62b29cd097477f6022640dac1d3
sdubrz <sdu2014@126.com>
2019-09-16 15:47:56 +0800

    第9章 聚类

---
+++ Ch06SVM/svm.py
@@ -0,0 +1,18 @@
+from sklearn import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def svm(X, label, C, kernel='linear'):
+    """
+
+    :param X: 样本数据矩阵， 每一行是一个样本点
+    :param label: 样本类别标签，分别是1和-1
+    :param C: 控制软间隔的常数
+    :param kernel: 核的类型，默认是线性核
+    :return:
+    """
+    (n, m) = X.shape
+
+
+def smo()
